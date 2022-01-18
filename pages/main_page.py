ae4f2dbf029b44cd258b78c15a33645971098a2c
Evgeny Malakhov <emalakhov@aaa.bbb>
2022-01-19 00:25:36 +0300

    4.2 step 10

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,9 +6,11 @@ from .login_page import LoginPage
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-       login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-       login_link.click()
-       # return LoginPage(browser=self.browser, url=self.browser.current_url)
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        login_link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
+        #alert = self.browser.switch_to.alert
+        #alert.accept()
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), f"Login link is not presented"
