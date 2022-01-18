54d939c165087317815c7a9c712e1509014bf1b6
Evgeny Malakhov <emalakhov@aaa.bbb>
2022-01-19 00:01:41 +0300

    add locators.py, login_page.py

---
+++ pages/locators.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+    REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
